d851d42247698e899aab1ad3c9723ac7241f6d97
Michal Kouril <michal@acai.local>
2026-03-20 22:29:31 -0400

    Increase default JVM heap

--- docker/run-local.sh
+++ docker/run-local.sh
@@ -5,7 +5,7 @@ cd /app/deepPhosAPI
 /opt/deepphos-env/bin/python predict.py &
 
 cd /app
-JAVA_HEAP_OPTS="${JAVA_HEAP_OPTS:--Xms512m -Xmx4g}"
+JAVA_HEAP_OPTS="${JAVA_HEAP_OPTS:--Xms1g -Xmx8g}"
 JAVA_TOOL_OPTIONS="--add-opens=java.base/java.lang=ALL-UNNAMED --add-opens=java.base/java.lang.reflect=ALL-UNNAMED --add-opens=java.base/java.io=ALL-UNNAMED --add-opens=java.base/java.util=ALL-UNNAMED" \
   java ${JAVA_HEAP_OPTS} -jar /app/pln.jar --server.port=8091 &
 
